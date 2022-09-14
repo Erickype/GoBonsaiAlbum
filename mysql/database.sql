f450394686d75a584e1209c997505ce9125dcf65
Erickype <erickype@hotmail.com>
2022-09-14 12:22:11 -0500

    Schema users and user, friendship tables sql created

--- mysql/database.sql
+++ mysql/database.sql
@@ -1,2 +1,23 @@
 create database if not exists test;
 create table test (id int primary key, name text);
+
+CREATE SCHEMA `users`;
+
+CREATE TABLE `users`.`user` (
+                                `id` int PRIMARY KEY AUTO_INCREMENT,
+                                `userName` varchar(30),
+                                `userLastname` varchar(30),
+                                `userNickname` varchar(15),
+                                `createdAt` datetime
+);
+
+CREATE TABLE `users`.`friendship` (
+                                      `firstUser` int,
+                                      `secondUser` int,
+                                      `createdAt` datetime,
+                                      PRIMARY KEY (`firstUser`, `secondUser`)
+);
+
+ALTER TABLE `users`.`friendship` ADD FOREIGN KEY (`firstUser`) REFERENCES `users`.`user` (`id`);
+
+ALTER TABLE `users`.`friendship` ADD FOREIGN KEY (`secondUser`) REFERENCES `users`.`user` (`id`);
