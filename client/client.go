6593b96d74342144c16bdb6056eb89c1704fd42f
Erickype <erickype@hotmail.com>
2022-09-16 12:14:40 -0500

    Use of a function for CreateUser in client and replace of the deprecated method grpc.WithInsecure()

--- client/client.go
+++ client/client.go
@@ -5,10 +5,11 @@ import (
 	"fmt"
 	pbUsers "github.com/Erickype/GoBonsaiAlbum/gRPC/users"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 
 	if err != nil {
 		panic("Connection error:  " + err.Error())
@@ -16,9 +17,13 @@ func main() {
 
 	clientService := pbUsers.NewUsersClient(conn)
 
+	CreateUser(clientService)
+}
+
+func CreateUser(clientService pbUsers.UsersClient) {
 	res, err := clientService.CreateUser(context.Background(), &pbUsers.CreateUserReq{
 		UserName:     "Erick",
-		UserLastname: "Carrasco",
+		UserLastname: "Carreras",
 		UserNickname: "Erickype",
 	})
 	if err != nil {
